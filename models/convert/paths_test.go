b14245b8281a797fae4514c1bd3dc71811b6fd1e
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2021-06-15 08:19:49 +0200

    Add convert package unit tests (#186)

---
+++ models/convert/paths_test.go
@@ -0,0 +1,89 @@
+// Copyright 2021 Alvalor S.A.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package convert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/onflow/flow-go/ledger"
+)
+
+func TestPathsToBytes(t *testing.T) {
+	path1b := []byte("aac513eb1a0457700ac3fa8d292513e1")
+	path2b := []byte("1454ae2420513f79f6a5e8396d033369")
+	path3b := []byte("e91a3eb997752b78bab0bc31e30b1e30")
+
+	var path1, path2, path3 ledger.Path
+	copy(path1[:], path1b)
+	copy(path2[:], path2b)
+	copy(path3[:], path3b)
+
+	paths := []ledger.Path{path1, path2, path3}
+
+	got := PathsToBytes(paths)
+
+	assert.Equal(t, [][]byte{path1b, path2b, path3b}, got)
+}
+
+func TestBytesToPaths(t *testing.T) {
+	t.Run("nominal case", func(t *testing.T) {
+		path1b := []byte("aac513eb1a0457700ac3fa8d292513e1")
+		path2b := []byte("1454ae2420513f79f6a5e8396d033369")
+		path3b := []byte("e91a3eb997752b78bab0bc31e30b1e30")
+
+		var path1, path2, path3 ledger.Path
+		copy(path1[:], path1b)
+		copy(path2[:], path2b)
+		copy(path3[:], path3b)
+
+		wantPaths := []ledger.Path{path1, path2, path3}
+
+		bb := [][]byte{path1b, path2b, path3b}
+
+		got, err := BytesToPaths(bb)
+
+		assert.NoError(t, err)
+		assert.Equal(t, wantPaths, got)
+	})
+
+	t.Run("incorrect-length paths should fail", func(t *testing.T) {
+		invalidPath := []byte("1a0457700")
+
+		bb := [][]byte{invalidPath}
+		_, err := BytesToPaths(bb)
+
+		assert.Error(t, err)
+	})
+
+	t.Run("empty paths should fail", func(t *testing.T) {
+		invalidPath := []byte("")
+
+		bb := [][]byte{invalidPath}
+		_, err := BytesToPaths(bb)
+
+		assert.Error(t, err)
+	})
+
+	t.Run("non-hex path should fail", func(t *testing.T) {
+		invalidPath := []byte("not even hex")
+
+		bb := [][]byte{invalidPath}
+		_, err := BytesToPaths(bb)
+
+		assert.Error(t, err)
+	})
+}
---
+++ models/convert/types_test.go
@@ -0,0 +1,39 @@
+// Copyright 2021 Alvalor S.A.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package convert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/onflow/flow-go/model/flow"
+)
+
+func TestTypesToStrings(t *testing.T) {
+	t1 := "test1"
+	t2 := "test2"
+	t3 := "test3"
+	t4 := "test4"
+
+	typ1 := flow.EventType(t1)
+	typ2 := flow.EventType(t2)
+	typ3 := flow.EventType(t3)
+	typ4 := flow.EventType(t4)
+
+	ss := TypesToStrings([]flow.EventType{typ1, typ2, typ3, typ4})
+
+	assert.Equal(t, []string{t1, t2, t3, t4}, ss)
+}
