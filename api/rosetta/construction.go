02e2a1308e135f2686b76b994b7f1b21f27fd0ee
Aleksandar Č <pathlidge.ann+github@gmail.com>
2021-08-04 11:58:53 +0200

    Construction API base code and Preprocess endpoint (#323)
    * Construction API code + Preprocess endpoint
    
    * Separating import groups
    
    * Implementing code review suggestions
    
    * Minor tweaks - comment updates
    
    * Adding issue links for TODOs

---
+++ api/rosetta/construction.go
@@ -0,0 +1,60 @@
+// Copyright 2021 Optakt Labs OÜ
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package rosetta
+
+const (
+	// FlowSignatureAlgorithm is the signing algorithm supported by Flow.
+	FlowSignatureAlgorithm = "ecdsa"
+)
+
+// TODO: Separate Data and Construction APIs to separate packages.
+// => https://github.com/optakt/flow-dps/issues/330
+
+// TODO: Sender is also the proposer and the payer. Consider allowing more granular control.
+// => https://github.com/optakt/flow-dps/issues/331
+
+// TODO: Specify Gas limit for the transaction.
+// => https://github.com/optakt/flow-dps/issues/332
+
+// FIXME: get account sequence number for the metadata endpoint (uses Access API)
+
+// Construction implements the Rosetta Construction API specification.
+// See https://www.rosetta-api.org/docs/construction_api_introduction.html
+type Construction struct {
+	config Configuration
+	parser Parser
+
+	// Retriever is used to retrieve the latest block ID. This is needed
+	// since the transactions require a reference block ID, so that
+	// their validity or expiration can be determined.
+	retriever Retriever
+
+	// Client is used to submit the constructed transaction to the Flow network.
+	client Client
+}
+
+// NewConstruction creates a new instance of the Construction API using the given configuration
+// to handle transaction construction requests.
+func NewConstruction(config Configuration, parser Parser, retriever Retriever, client Client) *Construction {
+
+	c := Construction{
+		config:    config,
+		parser:    parser,
+		retriever: retriever,
+		client:    client,
+	}
+
+	return &c
+}
