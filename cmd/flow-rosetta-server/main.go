62682aeba8bc29ce14d613bb372f449b7c13438f
Maelkum <pathlidge.ann+github@gmail.com>
2021-08-23 11:09:19 +0200

    Reorder endpoints

--- cmd/flow-rosetta-server/main.go
+++ cmd/flow-rosetta-server/main.go
@@ -179,9 +179,9 @@ func run() int {
 	server.POST("/construction/metadata", constructCtrl.Metadata)
 	server.POST("/construction/payloads", constructCtrl.Payloads)
 	server.POST("/construction/parse", constructCtrl.Parse)
+	server.POST("/construction/combine", constructCtrl.Combine)
 	server.POST("/construction/hash", constructCtrl.Hash)
 	server.POST("/construction/submit", constructCtrl.Submit)
-	server.POST("/construction/combine", constructCtrl.Combine)
 
 	// This section launches the main executing components in their own
 	// goroutine, so they can run concurrently. Afterwards, we wait for an
