944bb6e912fdf83f61b5a026fa634c8849cd888a
Max Wolter <awfm9@protonmail.com>
2021-09-14 12:22:32 +0200

    Update build tags with new version

--- api/dps/server_integration_test.go
+++ api/dps/server_integration_test.go
@@ -12,7 +12,8 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-//+build integration
+//go:build integration
+// +build integration
 
 package dps_test
 
