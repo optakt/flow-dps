ab2d1663f5f27703526ba58cd2778af7186f1fe0
Max Wolter <awfm9@protonmail.com>
2021-09-22 20:45:15 +0200

    Skip bootstrapping protocol state when already done

--- service/initializer/protocol_state.go
+++ service/initializer/protocol_state.go
@@ -16,6 +16,7 @@ package initializer
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 
@@ -24,13 +25,25 @@ import (
 	"github.com/onflow/flow-go/module/metrics"
 	protocol "github.com/onflow/flow-go/state/protocol/badger"
 	"github.com/onflow/flow-go/state/protocol/inmem"
+	"github.com/onflow/flow-go/storage"
 	cache "github.com/onflow/flow-go/storage/badger"
+	"github.com/onflow/flow-go/storage/badger/operation"
 )
 
 // ProtocolState initializes the Flow protocol state in the given database. The
 // code is inspired by the related unexported code in the Flow Go code base:
 // https://github.com/onflow/flow-go/blob/v0.21.0/cmd/bootstrap/cmd/finalize.go#L452
 func ProtocolState(file io.Reader, db *badger.DB) error {
+
+	// If we already have a root heigth, skip bootstrapping.
+	var root uint64
+	err := db.View(operation.RetrieveRootHeight(&root))
+	if err != nil && !errors.Is(err, storage.ErrNotFound) {
+		return fmt.Errorf("could not check root: %w", err)
+	}
+	if err == nil {
+		return nil
+	}
 
 	// Load the protocol snapshot from disk.
 	data, err := io.ReadAll(file)
