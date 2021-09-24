635dfaa52621ab6e73e5899e73026e86935a73bd
Max Wolter <awfm9@protonmail.com>
2021-09-24 13:59:05 +0200

    Add code comments to catchup blocks initializer

--- service/initializer/catchup_blocks.go
+++ service/initializer/catchup_blocks.go
@@ -27,12 +27,22 @@ import (
 	"github.com/optakt/flow-dps/models/dps"
 )
 
+// CatchupBlocks will determine, based on what is in the protocol state and
+// index databases, which blocks we need to download the execution records for
+// in order to properly resume catching up with consensus.
 func CatchupBlocks(db *badger.DB, read dps.Reader) ([]flow.Identifier, error) {
 
+	// We need to know for which blocks we don't need the execution records
+	// anymore, which is basically up to the last indexed block.
 	indexed, err := read.Last()
 	if err != nil && !errors.Is(err, badger.ErrKeyNotFound) {
 		return nil, fmt.Errorf("could not get last indexed: %w", err)
 	}
+
+	// If there is no last indexed block, we should start downloading execution
+	// records just after root height (for all of the blocks), so we put the
+	// last indexed height at root. If there is no root height, we don't need
+	// to catch up with anything, because the protocol state is also empty.
 	if errors.Is(err, badger.ErrKeyNotFound) {
 		var root uint64
 		err = db.View(operation.RetrieveRootHeight(&root))
@@ -45,6 +55,11 @@ func CatchupBlocks(db *badger.DB, read dps.Reader) ([]flow.Identifier, error) {
 		indexed = root
 	}
 
+	// Next, we check what the last finalized height in the protocol state is.
+	// If we can't find it, we don't have to catch up with anything; it's
+	// possible we are starting from scratch, or that we removed the protocol
+	// state without removing the index. In that case, we will simply re-index
+	// everything as we are syncing the protocol state.
 	var finalized uint64
 	err = db.View(operation.RetrieveFinalizedHeight(&finalized))
 	if errors.Is(err, storage.ErrNotFound) {
@@ -54,6 +69,10 @@ func CatchupBlocks(db *badger.DB, read dps.Reader) ([]flow.Identifier, error) {
 		return nil, fmt.Errorf("could not get last finalized: %w", err)
 	}
 
+	// We can now step from the first height after the indexed height to the
+	// finalized height and collect all the block IDs on the way. These can then
+	// be queueed in the cloud streamer to download the block records for blocks
+	// that have not yet been indexed in the correct order.
 	var blockIDs []flow.Identifier
 	for height := indexed + 1; height <= finalized; height++ {
 		var blockID flow.Identifier
