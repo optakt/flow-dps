a219d45dcf6552fbd1b2b4c2056389a7c4895b0e
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2023-03-08 13:47:51 -0800

    parse checkpoint file

--- service/mapper/state.go
+++ service/mapper/state.go
@@ -16,32 +16,39 @@ package mapper
 
 import (
 	"math"
+	"path/filepath"
 
 	"github.com/onflow/flow-go/ledger"
 )
 
 // State is the state machine's state for the current block being processed
 type State struct {
+	// TODO: remove forest
 	forest             Forest
 	status             Status
 	height             uint64 // the height to be indexed
 	updates            []*ledger.TrieUpdate
 	registers          map[ledger.Path]*ledger.Payload
 	checkpointDir      string // the checkpoint file for bootstrapping
-	checkpointFileName string // TODO: read these values from flagCheckpoint
+	checkpointFileName string
 	done               chan struct{}
 }
 
 // EmptyState returns a new empty state that uses the given forest.
-func EmptyState(forest Forest) *State {
+// TODO: remove forest
+func EmptyState(forest Forest, checkpointFile string) *State {
+
+	dir, fileName := filepath.Split(checkpointFile)
 
 	s := State{
-		forest:    forest,
-		status:    StatusInitialize,
-		height:    math.MaxUint64,
-		registers: make(map[ledger.Path]*ledger.Payload),
-		updates:   make([]*ledger.TrieUpdate, 0),
-		done:      make(chan struct{}),
+		forest:             forest,
+		status:             StatusInitialize,
+		height:             math.MaxUint64,
+		registers:          make(map[ledger.Path]*ledger.Payload),
+		updates:            make([]*ledger.TrieUpdate, 0),
+		checkpointDir:      dir,
+		checkpointFileName: fileName,
+		done:               make(chan struct{}),
 	}
 
 	return &s
