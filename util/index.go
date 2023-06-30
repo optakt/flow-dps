c829d0b0f38e63830e24d9dd1b413682133f58a3
Amlandeep Bhadra <amlandeep1912@gmail.com>
2023-06-30 09:39:10 -0400

    fix lint

--- util/index.go
+++ util/index.go
@@ -20,14 +20,10 @@ func ValidateHeightIndexed(reader archive.Reader, height uint64) error {
 func ValidateRegisterHeightIndexed(reader archive.Reader, height uint64) error {
 	h, err := reader.LatestRegisterHeight()
 	if err != nil {
-		return fmt.Errorf("could not get last indexed height for Archive node")
-	}
-	root, err := reader.First()
-	if height < root {
-		return fmt.Errorf("the requested height (%d) is below spork root height (%d)", height, root)
+		return fmt.Errorf("could not get last indexed register height for Archive node")
 	}
 	if height > h {
-		return fmt.Errorf("the requested height (%d) is beyond the highest indexed height(%d)", height, h)
+		return fmt.Errorf("the requested height (%d) is beyond the highest indexed height(%d) for registers", height, h)
 	}
 	return nil
 }
