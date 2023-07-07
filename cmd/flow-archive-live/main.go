88bcc173d8f7f8e5e1d43fe4997b5c69bb94e801
Amlandeep Bhadra <amlandeep1912@gmail.com>
2023-07-07 07:50:51 -0400

    try retrieving chainID from most recent indexed block

--- cmd/flow-archive-live/main.go
+++ cmd/flow-archive-live/main.go
@@ -529,7 +529,7 @@ func run() int {
 func getChainId(read *index.Reader) (flowModel.ChainID, error) {
 	chainID := flowModel.Emulator
 
-	f, err := read.First()
+	f, err := read.Last()
 	if err != nil {
 		return chainID, err
 	}
