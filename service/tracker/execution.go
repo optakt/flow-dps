04f363400e6565d2e67504b4099b969daa9f31f1
Amlandeep Bhadra <amlandeep1912@gmail.com>
2023-08-01 15:50:42 -0400

    update to check size for nil pointers

--- service/tracker/execution.go
+++ service/tracker/execution.go
@@ -194,9 +194,8 @@ func (e *Execution) processNext() error {
 		}
 		// hash search for matching update
 		for _, gcpUpdate := range record.TrieUpdates {
-			if gcpUpdate != nil {
-				hash := &gcpUpdate.RootHash
-				if hash != nil && !execTrieUpdates[gcpUpdate.String()].Equals(gcpUpdate) {
+			if gcpUpdate != nil && !gcpUpdate.IsEmpty() {
+				if !execTrieUpdates[gcpUpdate.String()].Equals(gcpUpdate) {
 					return fmt.Errorf("got %s mismatching trie updates between exec sync and GCP", gcpUpdate.String())
 				}
 			}
